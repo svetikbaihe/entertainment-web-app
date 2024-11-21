e34b44cc3da1de4b1778beef6dd1f81b182a5b68
Світлана Ем <svitlanaem@MacBook-Air-Svitlana.local>
2024-11-21 17:36:31 +0900

    feat:typography

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginImport from "eslint-plugin-import";
 
 export default tseslint.config(
 	{ ignores: ["dist"] },
@@ -21,6 +22,7 @@ export default tseslint.config(
 		plugins: {
 			"react-hooks": reactHooks,
 			"react-refresh": reactRefresh,
+			import: eslintPluginImport,
 		},
 		rules: {
 			...reactHooks.configs.recommended.rules,
@@ -40,12 +42,10 @@ export default tseslint.config(
 						"index",
 						"unknown",
 					],
-					newlinesBetween: "always",
+					"newlines-between": "always",
 					alphabetize: {
 						order: "asc",
 						caseInsensitive: true,
-						/* sort in ascending order. Options: ['ignore', 'asc', 'desc'] */
-						/* ignore case. Options: [true, false] */
 					},
 					pathGroups: [
 						{
--- src/app/component.tsx
+++ src/app/component.tsx
@@ -1,10 +1,21 @@
-import { CssBaseline } from "@mui/material";
 import React, { StrictMode } from "react";
+
+import { CssBaseline, ThemeProvider, Typography } from "@mui/material";
+import { theme } from "@theme/theme";
 
 const App: React.FC = () => {
 	return (
 		<StrictMode>
-			<CssBaseline></CssBaseline>
+			<CssBaseline>
+				<ThemeProvider theme={theme}>
+					<Typography variant="h1">Lorem ipsum dolor sit amet</Typography>
+					<Typography variant="h2">Consectetuer adipiscing elit</Typography>
+					<Typography variant="h3">Donec odio quisque volutpat mattis eros</Typography>
+					<Typography variant="h4">Nullam malesuada erat ut turpis</Typography>
+					<Typography variant="body1">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</Typography>
+					<Typography variant="body2">Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</Typography>
+				</ThemeProvider>
+			</CssBaseline>
 		</StrictMode>
 	);
 };
