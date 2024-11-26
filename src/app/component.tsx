04f25c7d2135584faacba83636871dd931b1a4bf
Світлана Ем <svitlanaem@MacBook-Air-Svitlana.local>
2024-11-26 19:59:05 +0900

    Set 'useTabs' to false

--- src/app/component.tsx
+++ src/app/component.tsx
@@ -5,32 +5,32 @@ import { Container, CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "@theme/theme";
 
 const App: React.FC = () => {
-	return (
-		<StrictMode>
-			<ThemeProvider theme={theme}>
-				<CssBaseline>
-					<Container sx={{ margin: "20px auto" }}>
-						<Input
-							error
-							helperText="Can't be empty"
-							type="email"
-							placeholder="Email address"
-						/>
-						<Input
-							helperText="Can't be empty"
-							type="password"
-							placeholder="Password"
-						/>
-						<Input
-							helperText="Can't be empty"
-							type="password"
-							placeholder="Repeat Password"
-						/>
-					</Container>
-				</CssBaseline>
-			</ThemeProvider>
-		</StrictMode>
-	);
+  return (
+    <StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <Container sx={{ margin: "20px auto" }}>
+            <Input
+              error
+              helperText="Can't be empty"
+              type="email"
+              placeholder="Email address"
+            />
+            <Input
+              helperText="Can't be empty"
+              type="password"
+              placeholder="Password"
+            />
+            <Input
+              helperText="Can't be empty"
+              type="password"
+              placeholder="Repeat Password"
+            />
+          </Container>
+        </CssBaseline>
+      </ThemeProvider>
+    </StrictMode>
+  );
 };
 
 export default App;
